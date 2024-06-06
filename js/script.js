621e3e3a492ca8d0eb517ba117b254ef2aef1d8f
George Khalil <159185251+George-Khalil-29@users.noreply.github.com>
2024-06-06 18:03:37 +0000

    added the wind speed and if it's raining

--- js/script.js
+++ js/script.js
@@ -4,33 +4,51 @@
 // Created on: May 2024
 // This file contains the JS functions for index.html
 
-"use strict" 
+"use strict"
 
 // Function to fetch weather data from OpenWeatherMap API
 async function getWeather() {
-    const apiKey = 'fe1d80e1e103cff8c6afd190cad23fa5'; // Replace 'YOUR_API_KEY' with your OpenWeatherMap API key
-    const lat = 45.4211435;
-    const lon = -75.6900574;
-    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+  const apiKey = "fe1d80e1e103cff8c6afd190cad23fa5" // Replace 'YOUR_API_KEY' with your OpenWeatherMap API key
+  const lat = 45.4211435
+  const lon = -75.6900574
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`
 
-    try {
-        const response = await fetch(apiUrl);
-        const data = await response.json();
-        console.log(data);
+  try {
+    const response = await fetch(apiUrl)
+    const data = await response.json()
+    console.log(data)
 
-        const temperature = data.main.temp;
-        const description = data.weather[0].description;
-        const iconCode = data.weather[0].icon;
-        const iconUrl = `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
+    const temperature = Math.round(data.main.temp)
+    const description = data.weather[0].description
+    const iconCode = data.weather[0].icon
+    const iconUrl = `https://openweathermap.org/img/wn/${iconCode}@2x.png`
 
-        document.getElementById('temperature').innerText = `Temperature: ${temperature}°C`;
-        document.getElementById('weather-description').innerText = `Description: ${description}`;
-        document.getElementById('weather-icon').src = iconUrl;
+    // Display temperature, weather description, and icon
+    document.getElementById(
+      "temperature"
+    ).innerText = `Temperature: ${temperature}°C`
+    document.getElementById(
+      "weather-description"
+    ).innerText = `Description: ${description}`
+    document.getElementById("weather-icon").src = iconUrl
 
-    } catch (error) {
-        console.error('catch', error);
-    }
+    // Display humidity
+    const humidity = data.main.humidity
+    document.getElementById("humidity").innerText = `Humidity: ${humidity}%`
+
+    // Display wind speed
+    const windSpeed = data.wind.speed
+    document.getElementById(
+      "wind-speed"
+    ).innerText = `Wind Speed: ${windSpeed} m/s`
+
+    // Display rain
+    const rain = data.rain ? `${data.rain["1h"]} mm` : "No rain" // Check if rain data exists
+    document.getElementById("rain").innerText = `Rain: ${rain}`
+  } catch (error) {
+    console.error("catch", error)
+  }
 }
 
 // Initial call to fetch weather data when the page loads
-getWeather();
+getWeather()
