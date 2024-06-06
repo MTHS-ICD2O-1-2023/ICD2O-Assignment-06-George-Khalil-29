52f56624ef6b5acc511b730d51646b38fe603387
George Khalil <159185251+George-Khalil-29@users.noreply.github.com>
2024-06-06 17:55:32 +0000

    delete the h3 text

--- js/script.js
+++ js/script.js
@@ -1,5 +1,36 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 George Khalil All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: George, Khalil
+// Created on: May 2024
 // This file contains the JS functions for index.html
+
+"use strict" 
+
+// Function to fetch weather data from OpenWeatherMap API
+async function getWeather() {
+    const apiKey = 'fe1d80e1e103cff8c6afd190cad23fa5'; // Replace 'YOUR_API_KEY' with your OpenWeatherMap API key
+    const lat = 45.4211435;
+    const lon = -75.6900574;
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+
+    try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        console.log(data);
+
+        const temperature = data.main.temp;
+        const description = data.weather[0].description;
+        const iconCode = data.weather[0].icon;
+        const iconUrl = `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
+
+        document.getElementById('temperature').innerText = `Temperature: ${temperature}°C`;
+        document.getElementById('weather-description').innerText = `Description: ${description}`;
+        document.getElementById('weather-icon').src = iconUrl;
+
+    } catch (error) {
+        console.error('catch', error);
+    }
+}
+
+// Initial call to fetch weather data when the page loads
+getWeather();
